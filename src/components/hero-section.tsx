009aa480c4fa18a3fe0833d9584d22bfe96c7bb7
Shivang <dixitshiva12358@gmail.com>
2026-02-15 03:04:22 +0530

    updated

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -65,7 +65,6 @@ export default function HeroSection() {
                       },
                     },
                   },
-                  item: transitionVariants.item,
                 }}
                 className="mt-12"
               >
